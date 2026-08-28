c0b741f8cc6324b89a4477ab73873743be0df910
Alejandro Pérez Alcojol <138225282+Zero2680@users.noreply.github.com>
2026-08-29 00:53:51 +0200

    Update and rename firegame.py to prometheusflame.py

--- prometheusflame.py
+++ prometheusflame.py
@@ -64,7 +64,7 @@ class Bola(Objeto):
 
 	def Movimiento_Lluvia(self, enemigo):
 		if self.check_colisiones(enemigo):
-			enemigo.vidas -= 1
+			enemigo.vidas -= 40
 			self.y = -100
 		if self.y < 750:
 			if self.y == -100:
@@ -77,6 +77,7 @@ class Bola(Objeto):
 		global tent
 		if self.check_colisiones(enemigo) == True:
 			enemigo.vidas -= 1
+			self.x = 1300
 		if tent != 2:
 			if self.direccionx == 1:
 				self.x += 0.25
@@ -85,6 +86,17 @@ class Bola(Objeto):
 		if self.x > 1280 or self.x < 0:
 			tent = 2
 
+	def Movimiento_Moneda(self, enemigo):
+		if self.check_colisiones(enemigo) == True:
+			enemigo.puntuacion += 50
+			self.x = 1300
+		self.puntuacion += 1
+		if self.puntuacion == 1:
+			self.x = randrange(1, 13) * 100
+			self.y = randrange(1, 7) * 100
+		if self.puntuacion >= 5000:
+			self.x = 1300
+
 def get_font(size):
     return font.Font("menu_assets/font.ttf", size)
 
@@ -92,12 +104,22 @@ cave = transform.scale(image.load("firegame_images/cave.png").convert(), (1280, 
 totem1 = transform.scale(image.load("firegame_images/totem1.png"), (60, 100))
 totem2 = transform.scale(image.load("firegame_images/totem2.png"), (60, 100))
 totem3 = transform.scale(image.load("firegame_images/totem3.png"), (60, 100))
+totem4 = transform.scale(image.load("firegame_images/totem4.png"), (60, 100))
+totem5 = transform.scale(image.load("firegame_images/totem5.png"), (60, 100))
+totem6 = transform.scale(image.load("firegame_images/totem6.png"), (60, 100))
+totem7 = transform.scale(image.load("firegame_images/totem7.png"), (60, 100))
+totem8 = transform.scale(image.load("firegame_images/totem8.png"), (60, 100))
+totem9 = transform.scale(image.load("firegame_images/totem9.png"), (60, 100))
 ball1 = transform.scale(image.load("firegame_images/ball1.png"), (52, 50))
 ball2 = transform.scale(image.load("firegame_images/ball2.png"), (52, 50))
 ball3 = transform.scale(image.load("firegame_images/ball3.png"), (52, 50))
 waterfall1 = transform.scale(image.load("firegame_images/waterfall1.png"), (100, 720))
 waterfall2 = transform.scale(image.load("firegame_images/waterfall2.png"), (100, 720))
 waterfall3 = transform.scale(image.load("firegame_images/waterfall3.png"), (100, 720))
+coin1 = transform.scale(image.load("firegame_images/coin1.png"), (24, 24))
+coin2 = transform.scale(image.load("firegame_images/coin2.png"), (24, 24))
+coin3 = transform.scale(image.load("firegame_images/coin3.png"), (24, 24))
+coin4 = transform.scale(image.load("firegame_images/coin4.png"), (24, 24))
 
 def main_menu():
     while True:
@@ -106,7 +128,7 @@ def main_menu():
 
         MENU_MOUSE_POS = mouse.get_pos()
 
-        MENU_TEXT = get_font(75).render("FIRE GAME", True, (178, 64, 182))
+        MENU_TEXT = get_font(75).render("PROMETHEUS FLAME", True, (255, 252, 46))
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
         PLAY_BUTTON = Button(image=image.load("menu_assets/Play Rect.png"), pos=(640, 250), 
@@ -149,7 +171,6 @@ def options():
     while True:
         OPTIONS_MOUSE_POS = mouse.get_pos()
 
-        #screen.blit(BG, (0, 0))
         screen.fill((0, 0, 0)) 
 
         OPTIONS_TEXT = get_font(45).render("OPTIONS", True, (178, 64, 182))
@@ -191,10 +212,13 @@ def play():
     gota2 = Bola(0, -100, 20, 20, 0, 0, 0, (255, 125, 255), 1, 1)
     catarata1 = Bola(plataforma.x-250, 0, 20, 720, 1, 0, 0, (255,0,0), 1, 1)
     catarata2 = Bola(plataforma.x-250, 0, 20, 720, 1, 0, 0, (255,0,0), 1, 1)
+    moneda1 = Bola(1300, 0, 20, 20, 1, 0, 0, (255, 255, 0), 1, 1)
     timer_plataforma = 0
     timer_ball = 0
     timer_catarata = 0
     timer_catarata2 = 0
+    timer_moneda = 0
+    timer_moneda2 = 0
     z = -1
     while True:
         global tent
@@ -212,6 +236,24 @@ def play():
         gota2.Movimiento_Lluvia(plataforma)
         gota1.DibujarObjeto()
         gota2.DibujarObjeto()
+
+        timer_moneda += 1
+        timer_moneda2 += 1
+        if timer_moneda >= 5000:
+            moneda1.puntuacion = 0
+            timer_moneda = 0
+        moneda1.Movimiento_Moneda(plataforma)
+        moneda1.DibujarObjeto()
+        if timer_moneda2 <= 150:
+            screen.blit(coin1, (moneda1.x-2, moneda1.y-2))
+        elif timer_moneda2 <= 300:
+            screen.blit(coin2, (moneda1.x-2, moneda1.y-2))
+        elif timer_moneda2 <= 450:
+            screen.blit(coin3, (moneda1.x-2, moneda1.y-2))
+        else:
+            screen.blit(coin4, (moneda1.x-2, moneda1.y-2))
+            if timer_moneda2 >= 600:
+                timer_moneda2 = 0
 
         if timer_catarata == 5000: movimiento = True
         if movimiento == False and tent == 2: tent = 0
@@ -249,14 +291,34 @@ def play():
                 if timer_catarata2 >= 300:
                     timer_catarata2 = 0
 
-        if timer_plataforma <= 150:
-            screen.blit(totem1, (plataforma.x-14, plataforma.y-50))
-        elif timer_plataforma <= 300:
-            screen.blit(totem2, (plataforma.x-14, plataforma.y-50))
-        else:
-            screen.blit(totem3, (plataforma.x-14, plataforma.y-50))
-            if timer_plataforma >= 450:
-                timer_plataforma = 0
+        if plataforma.vidas >= 66:
+            if timer_plataforma <= 150:
+                screen.blit(totem1, (plataforma.x-14, plataforma.y-50))
+            elif timer_plataforma <= 300:
+                screen.blit(totem2, (plataforma.x-14, plataforma.y-50))
+            else:
+                screen.blit(totem3, (plataforma.x-14, plataforma.y-50))
+                if timer_plataforma >= 450:
+                    timer_plataforma = 0
+        elif plataforma.vidas >= 33:
+            if timer_plataforma <= 150:
+                screen.blit(totem4, (plataforma.x-14, plataforma.y-50))
+            elif timer_plataforma <= 300:
+                screen.blit(totem5, (plataforma.x-14, plataforma.y-50))
+            else:
+                screen.blit(totem6, (plataforma.x-14, plataforma.y-50))
+                if timer_plataforma >= 450:
+                    timer_plataforma = 0
+        elif plataforma.vidas >= 0:
+            if timer_plataforma <= 150:
+                screen.blit(totem7, (plataforma.x-14, plataforma.y-50))
+            elif timer_plataforma <= 300:
+                screen.blit(totem8, (plataforma.x-14, plataforma.y-50))
+            else:
+                screen.blit(totem9, (plataforma.x-14, plataforma.y-50))
+                if timer_plataforma >= 450:
+                    timer_plataforma = 0
+
         if timer_ball <= 150:
             screen.blit(ball1, (gota1.x-14, gota1.y-25))
             screen.blit(ball1, (gota2.x-14, gota2.y-25))
